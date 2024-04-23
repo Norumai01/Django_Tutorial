57de601a95b2bce1c8113e73dbe5e446fd00dab5
Norumai01 <johnnydinhnguyen2001@gmail.com>
2024-04-23 16:02:25 -0400

    Added contents for webpage, Chapter 4

--- Beginner/Chapter_4_Message_Board/posts/admin.py
+++ Beginner/Chapter_4_Message_Board/posts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+# Allow admin to view the Post section.
+admin.site.register(Post)
--- Beginner/Chapter_4_Message_Board/posts/models.py
+++ Beginner/Chapter_4_Message_Board/posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    # Stores text type within the database.
+    text = models.TextField()
+
+    # Add this str function to change Posts title.
+    def __str__(self):
+        return self.text[:50]
